57e7a34734ab90132665c5b1db392a6695e8ac87
Owen Blacker <owen@blacker.me.uk>
2024-04-03 12:11:02 +0100

    Add iteration 2 of the Boutique Inventory exercise

--- boutique-inventory/boutique_inventory.rb
+++ boutique-inventory/boutique_inventory.rb
@@ -2,27 +2,38 @@ class BoutiqueInventory
   def initialize(items)
     @items = items
   end
+
   def item_names
-    @items.map { |item| item[:name] }.sort
+    items.map { |item| item[:name] }.sort
   end
+
   def cheap
-    @items.select { |item| item[:price] < 30.0 }
+    items.select { |item| item[:price] < 30.0 }
   end
+
   def out_of_stock
-    @items.select { |item| item[:quantity_by_size] == {} }
+    items.select { |item| item[:quantity_by_size] == {} }
   end
+
   def stock_for_item(name)
     #debug @items.select { |item| item[:name] == name }
     
-    @items.select { |item|
+    items.select { |item|
         item[:name] == name
     }[0][:quantity_by_size]
   end
+
   def total_stock
-    items_with_stock = @items.select { |item| !item[:quantity_by_size].empty? }
-    quantities = items_with_stock.map { |item| item[:quantity_by_size].values }
-    quantities.flatten.sum
+    # items_with_stock = items.select { |item| !item[:quantity_by_size].empty? }
+    # quantities = items_with_stock.map { |item| item[:quantity_by_size].values }
+    # quantities.flatten.sum
+
+    # Model code from "Boutique Inventory Improvements" exercise:
+    items.sum do |item|
+      item[:quantity_by_size].values.sum
+    end
   end
+
   private
   attr_reader :items
 end
