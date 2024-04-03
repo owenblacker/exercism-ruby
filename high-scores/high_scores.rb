b7fefdc00cab1ac377bc698e0998341fa2fc557b
Owen Blacker <owen@blacker.me.uk>
2024-04-03 12:18:22 +0100

    Add iteration 2 of the High Scores exercise

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -11,16 +11,20 @@ class HighScores
   def initialize(scores)
     @scores = scores
   end
+
   def latest
     scores[-1]
   end
+
   def personal_best
     scores.max
   end
+
   def personal_top_three
     scores.max(3)
   end
+
   def latest_is_personal_best?
-    scores[-1] == scores.max
+    latest == personal_best
   end
 end
